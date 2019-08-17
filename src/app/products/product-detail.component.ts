7aa912acce5ceaf74c16463f618f548d7a676deb
hasanmertmercan <mercan.hasanmert@gmail.com>
2019-08-17 23:04:08 +0200

    small-changes

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -7,7 +7,7 @@ import { IProduct } from './product';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-  pageTitle: string = "Product Detail";
+  pageTitle: string = "Product Detail ";
   product: IProduct;
   constructor(private route: ActivatedRoute, private router: Router) {}
 
